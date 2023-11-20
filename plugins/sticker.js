45d2d3f6550bc142a79dfb4e758313b117a953df
SNT Oficial <124302047+TIKIof@users.noreply.github.com>
2023-11-19 21:31:47 -0300

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -23,7 +23,7 @@ let img = await q.download?.()
 stiker = await mp4ToWebp(img, { pack: packname || global.packname, author: author || global.author })
 } else if (args[0] && isUrl(args[0])) {
 stiker = await createSticker(false, args[0], '', author, 20)
-} else throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾, 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾́𝙽 .𝚓𝚙𝚐 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁, 𝙳𝙴𝙱𝙴 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁 𝙾 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`
+} else throw `*[❗𝐈𝐍𝐅𝐎❗] RESPONDE A UN VIDEO, 𝙸𝙼𝙰𝙶𝙴𝙽 O INSERTE ENLACE DE UNA IMAGEN TERMINACION .𝚓𝚙𝚐 EL CUAL SERA CONVERTIDO EN STICKER, DEBE RESPONDER ${usedPrefix + command}*`
 } catch {
 try {
 stiker = await sticker(img, false, global.packname, global.author)
