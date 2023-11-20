dab411b983a9d66433d2dc95924ef4d6e04b3362
SNT Oficial <124302047+TIKIof@users.noreply.github.com>
2023-11-19 21:09:19 -0300

    hola.js

--- plugins/infohost.js
+++ plugins/infohost.js
@@ -1,21 +1,5 @@
 let handler = async (m, { conn }) => {
-m.reply(`*Hola! Buscas la mejor alternativa a Heroku, termux, replit, etc. de manera gratuita o de paga?*
-
-*- Esta es tu mejor opción para tener tu Bot, pagina web, servidor de minecraft o algun otro proyecto activo 24/7*
-
-*Incluso mejor que otros servicios como los antes mencionados, ¿Muy lento tu nokia y necesitas tener activado tu bot 24/7?*
-
-*[_Pagina principal_]*
-
-- boxmineworld.com
-
-*[_BoxMine Dash_]*
-
-- dash.boxmineworld.com
-
-*[_BoxMine Panel_]*
-
-- panel.boxmineworld.com`)
+m.reply(`*HOLA
 }
-handler.command = /^(infohost)$/i
+handler.command = /^(hola agromos)$/i
 module.exports = handler
