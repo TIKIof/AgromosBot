9d038d54715aa0e0d81aca1ce827dcdceacbd066
SNT Oficial <124302047+TIKIof@users.noreply.github.com>
2023-11-19 21:28:34 -0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,3 +1,4 @@
+
 function handler(m) {
 this.sendContact(m.chat, global.owner[1], this.getName(global.owner[1] + '@s.whatsapp.net'), m)
 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
