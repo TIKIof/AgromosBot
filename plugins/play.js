a291a55fa65b5a72da436d347d9781f1cc0abaa7
SNT Oficial <124302047+TIKIof@users.noreply.github.com>
2023-11-19 21:22:54 -0300

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 let fetch = require('node-fetch')
 let { youtubeSearch } = require('@bochilteam/scraper')
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Good Feeling - Flo Rida*`
+if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] FALTA EL NOMBRE DE LA CANCION, INGRESE EL NOMBRE DE UN VIDEO/TITULO DE UNA CANCION*\n\n*—◉ EJEMPLO𝙾:*\n*${usedPrefix + command} Good Feeling - Flo Rida*`
 try {
 let vid = (await youtubeSearch(text)).video[0]
 let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
@@ -29,10 +29,10 @@ externalAdReply: {
 showAdAttribution: true,
 mediaType: 'VIDEO',
 mediaUrl: null,
-title: '👑 𝐆𝐫𝐮𝐩𝐨_𝐃𝐞_𝐒𝐨𝐩𝐨𝐫𝐭𝐞 👑',
+title: '👑 GRUPO-DEL-BOT 👑',
 body: null,
 thumbnail: fs.readFileSync('./src/logo.png'),
-sourceUrl: `https://chat.whatsapp.com/FRkr7jJHSJA5OjVtE64dDs`
+sourceUrl: `https://chat.whatsapp.com/`
 }}}
 conn.sendMessage(m.chat, buttonMessage, { quoted: m }) 
 } catch {
