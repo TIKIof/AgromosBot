f1b970ec670a9670a599113c0056503b2e272f55
SNT Oficial <124302047+TIKIof@users.noreply.github.com>
2023-11-19 21:12:47 -0300

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,6 +1,6 @@
 let hispamemes = require("hispamemes")
 let handler = async (m, {command, conn}) => {
 const url = await hispamemes.meme() 
-conn.sendButtonImg(m.chat, url, `_${command}_`.trim(), author, '🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`, m)}
+conn.sendButtonImg(m.chat, url, `_${command}_`.trim(), author, '🔄 OTRO MEME 🔄', `/${command}`, m)}
 handler.command = /^(meme)$/i
 module.exports = handler
